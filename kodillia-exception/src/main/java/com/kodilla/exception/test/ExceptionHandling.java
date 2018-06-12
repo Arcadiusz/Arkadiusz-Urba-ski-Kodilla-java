81cc341919a07b538f9004bb430ae5cacc62363e
Arcadiusz <Arek94@onet.com.pl>
2018-06-12 18:11:08 +0100

    Added Second Challenge

---
+++ kodillia-exception/src/main/java/com/kodilla/exception/test/ExceptionHandling.java
@@ -0,0 +1,18 @@
+package com.kodilla.exception.test;
+
+public class ExceptionHandling extends Exception {
+
+    public static void main (String [] args ){
+
+        SecondChallenge challenge = new SecondChallenge();
+
+        try{
+        challenge.probablyIWillThrowException(1.2,1.6);
+            System.out.println("No exception found");
+        }catch(Exception e){
+            System.out.println("Exception found " + e);
+        } finally {
+            System.out.println("Method ececuted....");
+        }
+    }
+}
