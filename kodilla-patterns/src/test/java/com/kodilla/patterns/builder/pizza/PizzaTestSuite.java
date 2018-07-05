4121a51848f1bf69cbee7074fa0f7e67eac934da
Arcadiusz <Arek94@onet.com.pl>
2018-07-05 16:50:43 +0100

    Builder pattern pizza example

---
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/builder/pizza/PizzaTestSuite.java
@@ -0,0 +1,33 @@
+package com.kodilla.patterns.builder.pizza;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class PizzaTestSuite {
+    @Test
+    public void testPizzaNew() {
+        /* Test before using Builder design pattern
+        //Given
+        Pizza pizza = new Pizza("Thin", "Spicy", "Mushrooms", "Onion", "Ham");
+        System.out.println(pizza);
+        //When
+        int howManyIngredients = pizza.getIngredients().size();
+        //Then
+        Assert.assertEquals(3, howManyIngredients);
+        */
+
+        //Given
+        Pizza pizza = new Pizza.PizzaBuilder()
+                .ingredient("Onion")
+                .bottom("Thin")
+                .sauce("Spicy")
+                .ingredient("Ham")
+                .ingredient("Mushrooms")
+                .build();
+        System.out.println(pizza);
+        //When
+        int howManyIngredients = pizza.getIngredients().size();
+        //Then
+        Assert.assertEquals(3, howManyIngredients);
+    }
+}
