fb765a156151daa626fe0e92fad40f79b0523ffd
Arcadiusz <Arek94@onet.com.pl>
2018-09-25 15:08:57 +0200

    Adapter design pattern task and test added

---
+++ kodilla-patterns2/src/test/java/com/kodilla/patterns2/adapter/bookclasifier/MedianAdapterTestSuite.java
@@ -0,0 +1,32 @@
+package com.kodilla.patterns2.adapter.bookclasifier;
+
+import com.kodilla.patterns2.adapter.bookclasifier.librarya.Book;
+import com.kodilla.patterns2.adapter.bookclasifier.libraryb.BookSignature;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class MedianAdapterTestSuite {
+
+    @Test
+    public void publicationTestMedianYear() {
+
+        //Given
+        Set<Book> books = new HashSet<>();
+        books.add(new Book("author", "title", 1990,"1234"));
+        books.add(new Book("author1", "title1", 2000,"1233"));
+        books.add(new Book("author2", "title2", 2004,"1235"));
+        books.add(new Book("author3", "title3", 2008,"1236"));
+        books.add(new Book("author4", "title4", 1999,"1237"));
+
+        //When
+        MedianAdapter adapter = new MedianAdapter();
+        int median = adapter.publicationYearMedian(books);
+
+        //Then
+        Assert.assertEquals(2000,median);
+    }
+}
